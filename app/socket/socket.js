41a46026e6f83d9de86e76aab47da1cb2791a531
harshnanekar <nanekarharshal2@gmail.com>
2024-06-17 00:44:20 +0530

    message

--- app/socket/socket.js
+++ app/socket/socket.js
@@ -28,16 +28,16 @@ const initializeSocket = async (server) => {
     });
 
     socket.on("fetchMessage", async ({ userContactId, contact }) => {
-      console.log("user contact ", userContactId, contact);
 
       let chatData = await chat.getParticularChats(userContactId, contact);
+      await chat.changeMsgStatus(userContactId, contact);
+      
       let messages = chatData.rowCount > 0 ? chatData.rows : [];
 
       socket.emit("userList", { messages });
     });
 
     socket.on("private message", async ({ inputMessage, contact, userId }) => {
-      console.log("inside private message ");
       let message = await chat.sendMessage(inputMessage, contact, userId);
 
       let messageId = message.rows[0].message_id;
@@ -47,32 +47,37 @@ const initializeSocket = async (server) => {
       let receiverContact = recivedContact.rows[0].contact;
 
       if (users[receiverContact]) {
+
         socket.to(users[receiverContact]).emit("private message", {
           senderPhoneNumber: socket.userId,
           inputMessage : inputMessage,
-          messageLid : messageId
+          messageLid : messageId,
+          // chatUsers : chatUsers
         });
+
       }
 
       let chatData = await chat.getParticularChats(userId, contact);
-      // console.log("user message ", chatData.rows);
       let updateChat = chatData.rowCount > 0 ? chatData.rows : [];
       socket.emit("updatedChats", { updateChat });
+
+      let userChats = await chat.userChats(userId);
+      let chatUsers = userChats.rowCount > 0 ? userChats.rows : [];
+      socket.emit("newUserChatList", { chatUsers });
+
     });
 
     socket.on("updateMsgStatus",async ({messageLid,status}) => {
-     let updateStatus = await chat.updateMsgStatus(messageLid,status);
-    //  socket.messageLid = messageLid;
-    //  socket.join(messageLid)
-    //  userStatus[messageLid] = socket.id;
-
-    //  console.log('socket message Id ',socket.messageLid)
-    //  socket.to(userStatus[messageLid]).emit('updatedMsgStatus',{
-    //   messageLid : socket.messageLid,
-    //   status : status  
-    //  })
+      await chat.updateMsgStatus(messageLid,status);
     })
 
+    socket.on('sender Chats',async ({userId}) => {
+      let userChatMsg = await chat.userChats(userId);
+      let chatUserMsg = userChatMsg.rowCount > 0 ? userChatMsg.rows : [];
+      socket.emit('sender msg',{chatUserMsg});
+    })
+
+ 
   });
 };
 module.exports = initializeSocket;
